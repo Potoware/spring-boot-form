72515d5c0ec5a904f24f941adad76bc4cd44372e
Potoware <alejandropotosi@outlook.com>
2021-09-13 16:36:59 -0500

    Mapeando datos al pojo con el objeto

--- src/main/java/com/potoware/springboot/form/controller/FormController.java
+++ src/main/java/com/potoware/springboot/form/controller/FormController.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.potoware.springboot.form.models.domain.Usuario;
 
 @Controller
 public class FormController {
@@ -12,15 +13,15 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("titulo", "Usuarios - Registro");
+		
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String recibirForm(Model model, @RequestParam(name="username") String username, @RequestParam(name="password") String password, @RequestParam(name="email")  String email) {
+	public String recibirForm(Usuario usuario, Model model) {
+		
 		model.addAttribute("titulo","Resultado Formulario");
-		model.addAttribute("username",username);
-		model.addAttribute("password",password);
-		model.addAttribute("email",email);
+		model.addAttribute("usuario",usuario);
 		return "resultado";
 	}
 
