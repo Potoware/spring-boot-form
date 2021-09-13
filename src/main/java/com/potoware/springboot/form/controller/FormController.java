fbe450c17a23d054e770661b72cb1c3e8ac8b7cb
Potoware <alejandropotosi@outlook.com>
2021-09-13 16:21:42 -0500

    Metodo para recibir y procesar datos con el handler

--- src/main/java/com/potoware/springboot/form/controller/FormController.java
+++ src/main/java/com/potoware/springboot/form/controller/FormController.java
@@ -4,19 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
-		
+		model.addAttribute("titulo", "Usuarios - Registro");
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String recibirForm(Model model) {
-		
+	public String recibirForm(Model model, @RequestParam(name="username") String username, @RequestParam(name="password") String password, @RequestParam(name="email")  String email) {
+		model.addAttribute("titulo","Resultado Formulario");
+		model.addAttribute("username",username);
+		model.addAttribute("password",password);
+		model.addAttribute("email",email);
 		return "resultado";
 	}
 
