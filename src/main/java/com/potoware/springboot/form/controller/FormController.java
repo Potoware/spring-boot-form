836a685c3179e317972196ea47796070aa325ee7
Potoware <alejandropotosi@outlook.com>
2021-09-13 16:12:11 -0500

    Añadiendo vista de formulario

--- src/main/java/com/potoware/springboot/form/controller/FormController.java
+++ src/main/java/com/potoware/springboot/form/controller/FormController.java
@@ -1,10 +1,23 @@
 package com.potoware.springboot.form.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
 	
+	@GetMapping("/form")
+	public String form(Model model) {
+		
+		return "form";
+	}
 	
+	@PostMapping("/form")
+	public String recibirForm(Model model) {
+		
+		return "resultado";
+	}
 
 }
