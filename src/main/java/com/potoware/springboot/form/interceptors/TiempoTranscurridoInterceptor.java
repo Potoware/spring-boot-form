0ef28deab2d7acd3d4f236a2492c0403ab375a29
Potoware <alejandropotosi@outlook.com>
2021-09-14 17:39:45 -0500

    573. Introduccón a los interceptores Http

---
+++ src/main/java/com/potoware/springboot/form/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,7 @@
+package com.potoware.springboot.form.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
+	
+}
