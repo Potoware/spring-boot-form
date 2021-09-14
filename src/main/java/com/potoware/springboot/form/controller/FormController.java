1c58688d146ef155dd06d7f54bd543530d6a8b7c
Potoware <alejandropotosi@outlook.com>
2021-09-13 20:12:01 -0500

    536. El atributo object de thymeleaf en el elemento form

--- src/main/java/com/potoware/springboot/form/controller/FormController.java
+++ src/main/java/com/potoware/springboot/form/controller/FormController.java
@@ -1,8 +1,5 @@
 package com.potoware.springboot.form.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -28,11 +25,14 @@ public class FormController {
 	public String recibirForm(@Valid Usuario usuario, BindingResult result,Model model) {
 		model.addAttribute("titulo","Resultado Formulario");
 		if(result.hasErrors()) {
-			Map<String,String> errores = new HashMap<>();
+			
+			
+			/*Antigua forma
+			 * Map<String,String> errores = new HashMap<>();
 			result.getFieldErrors().forEach(err ->{
 				errores.put(err.getField(), "El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
 				});
-			model.addAttribute("error", errores);
+			model.addAttribute("error", errores);*/
 			return "form";
 		}
 		
